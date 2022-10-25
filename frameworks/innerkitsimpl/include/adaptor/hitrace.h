4378d866a3d2ee38ea613b0f8b47cbf8f5262c4c
wangxiyue <wangxiyue8@huawei.com>
2022-10-25 16:21:24 +0800

    删除DdsTrace依赖，用Hitrace替代
    Signed-off-by: wangxiyue <wangxiyue8@huawei.com>

---
+++ frameworks/innerkitsimpl/include/adaptor/hitrace.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef DATA_OBJECT_HITRACE_H
+#define DATA_OBJECT_HITRACE_H
+
+#include "hitrace_meter.h"
+
+namespace OHOS {
+namespace ObjectStore {
+class HiTrace final {
+public:
+    inline HiTrace(const std::string &value)
+    {
+        StartTrace(HITRACE_TAG_DISTRIBUTEDDATA, value);
+    }
+
+    inline ~HiTrace()
+    {
+        FinishTrace(HITRACE_TAG_DISTRIBUTEDDATA);
+    }
+};
+} // namespace ObjectStore
+} // namespace OHOS
+
+#endif // DATA_OBJECT_HITRACE_H
