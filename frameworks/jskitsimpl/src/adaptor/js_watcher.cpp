786daba3d6fd3a04d83a6c77cffef432dcc0b01c
“wangxiyue” <wangxiyue8@huawei.com>
2022-10-27 19:09:31 +0800

    modified code
    Signed-off-by: “wangxiyue” <wangxiyue8@huawei.com>

--- frameworks/jskitsimpl/src/adaptor/js_watcher.cpp
+++ frameworks/jskitsimpl/src/adaptor/js_watcher.cpp
@@ -270,6 +270,10 @@ bool ChangeEventListener::Add(napi_env env, napi_value handler)
 {
     if (!isWatched_ && object_ != nullptr) {
         std::shared_ptr<WatcherImpl> watcher = std::make_shared<WatcherImpl>(watcher_);
+        if (watcher == nullptr) {
+            LOG_ERROR("new %{public}s error", object_->GetSessionId().c_str());
+            return false;
+        }
         uint32_t ret = objectStore_->Watch(object_, watcher);
         if (ret != SUCCESS) {
             LOG_ERROR("Watch %{public}s error", object_->GetSessionId().c_str());
