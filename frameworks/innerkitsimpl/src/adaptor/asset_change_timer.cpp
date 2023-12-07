331a32e56a141d003545016a665716db3c943463
zph <zhaopenghui5@huawei.com>
2023-12-07 22:00:47 +0800

    update
    Signed-off-by: zph <zhaopenghui5@huawei.com>

--- frameworks/innerkitsimpl/src/adaptor/asset_change_timer.cpp
+++ frameworks/innerkitsimpl/src/adaptor/asset_change_timer.cpp
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 #define LOG_TAG "AssetChangeTimer"
-
 #include "asset_change_timer.h"
+
 #include "client_adaptor.h"
 #include "logger.h"
 #include "objectstore_errors.h"
@@ -25,7 +25,7 @@ AssetChangeTimer *AssetChangeTimer::instance = nullptr;
 
 AssetChangeTimer *AssetChangeTimer::GetInstance(FlatObjectStore *flatObjectStore)
 {
-    if (instance == nullptr){
+    if (instance == nullptr) {
         std::lock_guard<decltype(instanceMutex)> lockGuard(instanceMutex);
         if (instance == nullptr) {
             instance = new (std::nothrow) AssetChangeTimer(flatObjectStore);
@@ -40,37 +40,37 @@ AssetChangeTimer::AssetChangeTimer(FlatObjectStore *flatObjectStore)
     executor_ = std::make_shared<ExecutorPool>(MAX_THREADS, MIN_THREADS);
 }
 
-void AssetChangeTimer::OnAssetChanged(const std::string &sessionId, const std::string &assetKey,
-    std::shared_ptr<ObjectWatcher> watcher)
+void AssetChangeTimer::OnAssetChanged(
+    const std::string &sessionId, const std::string &assetKey, std::shared_ptr<ObjectWatcher> watcher)
 {
     StartTimer(sessionId, assetKey, watcher);
 }
 
-void AssetChangeTimer::StartTimer(const std::string &sessionId, const std::string &assetKey,
-    std::shared_ptr<ObjectWatcher> watcher)
+void AssetChangeTimer::StartTimer(
+    const std::string &sessionId, const std::string &assetKey, std::shared_ptr<ObjectWatcher> watcher)
 {
     std::string key = sessionId + ASSET_SEPARATOR + assetKey;
     std::lock_guard<decltype(mutex_)> lockGuard(mutex_);
     if (assetChangeTasks_.find(key) == assetChangeTasks_.end()) {
-        assetChangeTasks_[key] = executor_->Schedule(
-            std::chrono::milliseconds(WAIT_INTERVAL), ProcessTask(sessionId, assetKey, watcher));
+        assetChangeTasks_[key] =
+            executor_->Schedule(std::chrono::milliseconds(WAIT_INTERVAL), ProcessTask(sessionId, assetKey, watcher));
     } else {
         assetChangeTasks_[key] = executor_->Reset(assetChangeTasks_[key], std::chrono::milliseconds(WAIT_INTERVAL));
     }
 }
 
-std::function<void()> AssetChangeTimer::ProcessTask(const std::string &sessionId, const std::string &assetKey,
-    std::shared_ptr<ObjectWatcher> watcher)
+std::function<void()> AssetChangeTimer::ProcessTask(
+    const std::string &sessionId, const std::string &assetKey, std::shared_ptr<ObjectWatcher> watcher)
 {
     return [=]() {
-        LOG_DEBUG("Start working on a task, sessionId: %{public}s, assetKey: %{public}s", sessionId.c_str(), 
+        LOG_DEBUG("Start working on a task, sessionId: %{public}s, assetKey: %{public}s", sessionId.c_str(),
             assetKey.c_str());
         StopTimer(sessionId, assetKey);
         uint32_t status = HandleAssetChanges(sessionId, assetKey);
         if (status == SUCCESS) {
-            LOG_DEBUG("Asset change task end, start callback, sessionId: %{public}s, assetKey: %{public}s", 
+            LOG_DEBUG("Asset change task end, start callback, sessionId: %{public}s, assetKey: %{public}s",
                 sessionId.c_str(), assetKey.c_str());
-            watcher->OnChanged(sessionId, {assetKey});
+            watcher->OnChanged(sessionId, { assetKey });
         }
     };
 }
@@ -87,7 +87,7 @@ uint32_t AssetChangeTimer::HandleAssetChanges(const std::string &sessionId, cons
 {
     Asset assetValue;
     if (!GetAssetValue(sessionId, assetKey, assetValue)) {
-        LOG_ERROR("GetAssetValue assetValue is not complete, sessionId: %{public}s, assetKey: %{public}s", 
+        LOG_ERROR("GetAssetValue assetValue is not complete, sessionId: %{public}s, assetKey: %{public}s",
             sessionId.c_str(), assetKey.c_str());
         return ERR_DB_GET_FAIL;
     }
@@ -106,8 +106,8 @@ uint32_t AssetChangeTimer::HandleAssetChanges(const std::string &sessionId, cons
     }
     status = proxy->OnAssetChanged(flatObjectStore_->GetBundleName(), sessionId, deviceId, assetValue);
     if (status != SUCCESS) {
-        LOG_ERROR("OnAssetChanged failed status: %{public}d, sessionId: %{public}s, assetKey: %{public}s", 
-            status, sessionId.c_str(), assetKey.c_str());
+        LOG_ERROR("OnAssetChanged failed status: %{public}d, sessionId: %{public}s, assetKey: %{public}s", status,
+            sessionId.c_str(), assetKey.c_str());
     }
     return status;
 }
@@ -127,7 +127,6 @@ bool AssetChangeTimer::GetAssetValue(const std::string &sessionId, const std::st
     isComplete &=
         (flatObjectStore_->GetString(sessionId, assetKey + MODIFY_TIME_SUFFIX, assetValue.modifyTime) == SUCCESS);
     isComplete &= (flatObjectStore_->GetString(sessionId, assetKey + SIZE_SUFFIX, assetValue.size) == SUCCESS);
-
     if (isComplete) {
         assetValue.name = assetValue.name.substr(STRING_PREFIX_LEN);
         assetValue.uri = assetValue.uri.substr(STRING_PREFIX_LEN);
