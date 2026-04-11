e3207e6cb296d746fe3d71a0a360aefa7572976b
lvchao_2580 <lvchao28@h-partners.com>
2026-04-11 18:47:57 +0800

    update: 更新文件 ani_constructor.cpp
    Signed-off-by: lvchao_2580 <lvchao28@h-partners.com>

--- frameworks/ets/taihe/ohos.data.distributedDataObject/src/ani_constructor.cpp
+++ frameworks/ets/taihe/ohos.data.distributedDataObject/src/ani_constructor.cpp
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-#inedif DATA_OBJECT_FEATURE_L1
+#ifndef DATA_OBJECT_FEATURE_L1
 #include "ohos.data.commonType.ani.hpp"
 #endif
 #include "ohos.data.distributedDataObject.ani.hpp"
